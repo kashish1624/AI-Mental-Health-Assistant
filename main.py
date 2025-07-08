3ab7d8f0aa4a11d1fe1d1bc9c03555da6a2e18bf
kashish1624 <kashishgupta0216@gmail.com>
2025-07-09 02:49:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from gui.dashboard import launch_dashboard
 # Replace with actual user from login (we’ll pass this from login_window.py)
 logged_in_user = "temp_user@example.com"  # Placeholder
 
-create_dashboard(logged_in_user)
+launch_dashboard(logged_in_user)
 
